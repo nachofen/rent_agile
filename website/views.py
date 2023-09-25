d9f1c4862cfa9072fd040e7be0954e5f080b4f20
nachofen <124267664+nachofen@users.noreply.github.com>
2023-09-25 13:48:36 -0700

    multipictures working

---
+++ website/views.py
@@ -0,0 +1,172 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_required, login_user, logout_user, current_user
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
+from werkzeug.utils import secure_filename
+
+views = Blueprint('views', __name__)
+UPLOAD_FOLDER = os.path.abspath('website/static/img/uploads')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    """to validate allowed extensions"""
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@views.route('/')
+def home():
+    """home url"""
+    return render_template("index.html", user=current_user)
+
+@views.route('/host')
+@login_required
+def admin_home():
+    """admin home url"""
+    return render_template("indexp.html", user=current_user)
+
+@views.route('/agregar-vehiculo', methods=['GET', 'POST'])
+@login_required
+def agregar_vehiculo():
+    """add vehicle url"""
+    from .models import Auto, Imagenes_auto
+
+    if request.method == 'POST':
+        # Obtener datos del formulario
+        marca = request.form.get('marca')
+        modelo = request.form.get('modelo')
+        año = request.form.get('año')
+        categoria = request.form.get('categoria')
+        departamento = request.form.get('departamento')
+        tarifa = request.form.get('tarifa')
+        descripcion = request.form.get('descripcion')
+
+        nuevo_vehiculo = Auto(
+            marca=marca,
+            modelo=modelo,
+            año=año,
+            categoria=categoria,
+            departamento=departamento,
+            tarifa=tarifa,
+            descripcion=descripcion,
+            usuario_id=current_user.id  # Asociar el vehículo al usuario actual
+        )
+
+        # Agregar el nuevo vehículo a la lista de vehículos del usuario
+        current_user.autos.append(nuevo_vehiculo)
+
+        # Guardar provisionalmente el nuevo vehículo en la sesión de SQLAlchemy
+        db.session.add(nuevo_vehiculo)
+        db.session.commit()  # El vehículo se insertará en la base de datos y se generará el ID automáticamente
+
+        auto = Auto.query.get(nuevo_vehiculo.id_auto)
+
+        # Procesar la carga de imágenes
+        for image in request.files.getlist('images[]'):
+            if image.filename != '' and allowed_file(image.filename):
+                # Guardar la imagen en el directorio UPLOAD_FOLDER
+                filename = secure_filename(image.filename)
+                unique_filename = f"auto_{auto.id_auto}_{filename}"  # Nombre único de archivo
+                image.save(os.path.join(UPLOAD_FOLDER, unique_filename))
+
+                # Crear una entrada en la tabla Imagenes_auto para la imagen
+                nueva_imagen = Imagenes_auto(url=os.path.join('static/img/uploads', unique_filename), auto=auto)
+                db.session.add(nueva_imagen)
+
+        # Commit los cambios en la base de datos
+        db.session.commit()
+
+        flash('El nuevo vehículo se ha agregado con éxito!', category='success')
+        return redirect(url_for('views.update_info'))
+
+    return render_template("agregar-vehiculo.html", user=current_user)
+
+@views.route('/perfil')
+@login_required
+def perfil_usuario():
+    """perfil de usuario"""
+    print(current_user.nombre)
+    return render_template("perfil.html", user=current_user)
+
+departamentos = [
+    "Artigas",
+    "Canelones",
+    "Cerro Largo",
+    "Colonia",
+    "Durazno",
+    "Flores",
+    "Florida",
+    "Lavalleja",
+    "Maldonado",
+    "Montevideo",
+    "Paysandú",
+    "Río Negro",
+    "Rivera",
+    "Rocha",
+    "Salto",
+    "San José",
+    "Soriano",
+    "Tacuarembó",
+    "Treinta y Tres"
+]
+UPLOAD_FOLDER = 'website/static/img/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    """Función para validar extensiones de archivo permitidas"""
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@views.route('/mis-datos', methods=['GET', 'POST'])
+@login_required
+def update_info():
+    if request.method == 'POST':
+        # Obtener datos del formulario
+        email = request.form.get('email')
+        nombre = request.form.get('name')
+        apellido = request.form.get('apellido')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+        departamento = request.form.get('departamento')
+        direccion = request.form.get('direccion')
+        
+        if len(email) < 4:
+            flash('Email debe tener al menos 4 caracteres.', category='error')
+        elif len(nombre) < 3:
+            flash('Nombre debe tener al menos 3 caracteres.', category='error')
+        elif len(apellido) < 3:
+            flash('Apellido debe tener al menos 3 caracteres.', category='error')
+        elif password != password2:
+            flash('Las contraseñas no coinciden.', category='error')
+        elif len(password) < 6:
+            flash('Su contraseña debe tener al menos 6 caracteres.', category='error')
+        elif len(password2) < 6:
+            flash('Su contraseña debe tener al menos 6 caracteres.', category='error')
+        else:
+            if 'image' in request.files:
+                image = request.files['image']
+                if image.filename != '' and allowed_file(image.filename):
+                    # Guarda la imagen en el directorio UPLOAD_FOLDER
+                    filename = f"{current_user.id}_{secure_filename(image.filename)}"
+                    image.save(os.path.join(UPLOAD_FOLDER, filename))
+                    # Crear una URL relativa para la imagen
+                    relative_url = os.path.join('static/img/uploads', filename)
+
+                    # Actualizar el campo image_path del usuario
+                    current_user.image_path = relative_url
+                    
+
+            # Actualiza la información del usuario en la base de datos
+            current_user.email = email
+            current_user.nombre = nombre
+            current_user.apellido = apellido
+            current_user.password = generate_password_hash(password, method='sha256')
+            current_user.departamento = departamento
+            current_user.direccion = direccion
+            # Commit los cambios en la base de datos
+            db.session.commit()
+            
+            flash('Sus datos se han actualizado con éxito!', category='success')
+            return redirect(url_for('views.update_info'))
+
+    # Pasa los datos del usuario a la plantilla
+    return render_template('mis-datos.html', user=current_user, departamentos=departamentos)
