24de4d5e2814c3faebbf534b7703d4653047781d
binbashz <5732@holbertonstudents.com>
2023-10-26 20:35:27 -0300

    fix positions in divcard scroll-evnt at home

--- website/static/js/divscroll.js
+++ website/static/js/divscroll.js
@@ -7,25 +7,25 @@ function mostrarDivOculto() {
     // Obtén la posición vertical actual del scroll
     var scrollPosition = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
 
-    // Define la posición en la que deseas mostrar el div (por ejemplo, a 200 píxeles desde la parte superior)
-    var mostrarEnPosicion = 200;
+    // posición en la cual mostrar el  div (ej 150 píxeles desde la parte superior)
+    var mostrarEnPosicion = 500;
 
     if (scrollPosition >= mostrarEnPosicion && !shown) {
         anime({
             targets: divOculto,
-            translateY: [100, 0], // Cambia la posición vertical del div para que aparezca desde abajo
-            opacity: [0, 1], // Cambia la opacidad para que aparezca gradualmente
-            duration: 500, // Duración de la animación en milisegundos
-            easing: 'easeOutQuad' // Tipo de transición (puedes ajustarlo)
+            translateY: [100, 30], // Cambia la posición vertical del div para que aparezca desde abajo
+            opacity: [0, 4], // Cambia la opacidad para que aparezca gradualmente
+            duration: 700, // Duración de la animación en milisegundos
+            easing: 'easeOutQuad' // Tipo de transición 
         });
         shown = true;
     } else if (scrollPosition < mostrarEnPosicion && shown) {
         anime({
             targets: divOculto,
             translateY: [0, 100], // Cambia la posición vertical para que desaparezca hacia abajo
-            opacity: [1, 0], // Cambia la opacidad para que desaparezca gradualmente
-            duration: 500, // Duración de la animación en milisegundos
-            easing: 'easeOutQuad' // Tipo de transición (puedes ajustarlo)
+            opacity: [1, 0], 
+            duration: 400, 
+            easing: 'easeOutQuad' // Tipo de transición 
         });
         shown = false;
     }
