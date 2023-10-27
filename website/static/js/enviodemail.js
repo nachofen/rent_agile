e1ec040e0ac71dccc56ba44b77aabc49976f5507
binbashz <5732@holbertonstudents.com>
2023-10-27 13:33:37 -0300

    alert messsage  success

--- website/static/js/enviodemail.js
+++ website/static/js/enviodemail.js
@@ -12,7 +12,7 @@ document.getElementById('form')
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.value = 'Send Email';
-      alert('Sent!');
+      alert('Mensaje enviado');
     }, (err) => {
       btn.value = 'Send Email';
       alert(JSON.stringify(err));
