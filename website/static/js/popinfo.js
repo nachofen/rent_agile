398ffa5d39311a165a419e6b03d89d9b9695ff31
binbashz <5732@holbertonstudents.com>
2023-11-12 11:52:12 -0300

    /ver-vehiculo style

---
+++ website/static/js/popinfo.js
@@ -0,0 +1,17 @@
+// pop up info importante
+
+
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
+
+// Oculta el pop-up y el overlay al cargar la página
+document.addEventListener("DOMContentLoaded", function() {
+    closePopup();
+});
