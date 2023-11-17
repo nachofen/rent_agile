32f75273b2d0d85a629de431303e33a0f624c921
nachofen <124267664+nachofen@users.noreply.github.com>
2023-11-17 09:02:14 -0800

    mas informacion

--- website/static/js/popinfo.js
+++ website/static/js/popinfo.js
@@ -1,5 +1,4 @@
 // pop up info importante
-
 function openPopup() {
     document.getElementById("popup").style.display = "block";
     document.getElementById("overlay").style.display = "block";
@@ -13,4 +12,12 @@ function closePopup() {
 // Oculta el pop-up y el overlay al cargar la página
 document.addEventListener("DOMContentLoaded", function() {
     closePopup();
+
+    // Oculta el pop-up y el overlay al hacer clic fuera del pop-up
+    const overlay = document.getElementById("overlay");
+    overlay.addEventListener("click", function(event) {
+        if (event.target === overlay) {
+            closePopup();
+        }
+    });
 });
